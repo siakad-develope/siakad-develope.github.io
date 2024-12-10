63d34bb6c21f6bc7b9db5f44dcbaa3bc2afb401f
KostsPlayer <azka.nuril070@gmail.com>
2024-12-10 16:15:36 +0700

    testing deploy

---
+++ src/routes/index.jsx
@@ -0,0 +1,26 @@
+import { lazy } from "react";
+import Loadable from "./Loadable";
+import { AuthRoutes } from "./auth";
+import { PublicRoutes } from "./public";
+import { DashboardRoutes } from "./dashboard";
+import { ComponentRoutes } from "./components"; // GLOBAL ERROR PAGE
+import { Navigate } from "react-router-dom";
+
+const ErrorPage = Loadable(lazy(() => import("@/pages/404"))); // LANDING / INITIAL PAGE
+
+const Landing = Loadable(lazy(() => import("@/pages/landing")));
+export const routes = () => {
+  return [// INITIAL / INDEX PAGE
+  {
+    path: "/",
+    element: <Navigate to="/dashboard" />
+  }, // GLOBAL ERROR PAGE
+  {
+    path: "*",
+    element: <ErrorPage />
+  }, // AUTHENTICATION PAGES ROUTES & DIFFERENT AUTH DEMO PAGES ROUTES
+  ...AuthRoutes, // COMPONENTS PAGES ROUTES
+  ...ComponentRoutes, // INSIDE DASHBOARD PAGES ROUTES
+  ...DashboardRoutes, // PAGES ROUTES
+  ...PublicRoutes];
+};
